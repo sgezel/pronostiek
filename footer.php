c2be476125b23efdc4fb7b59cdcb00ddcb4f3ca4
Sandor <sandor@codepage.be>
2018-03-28 23:02:30 +0200

    Add footer to the template

--- footer.php
+++ footer.php
@@ -1,3 +1,17 @@
+  <footer class="footer bg-dark">
+      <div class="container">
+      	<span class="left">
+      		&copy; Sandor Gezel & Dieter Cams <?= date("Y"); ?>
+          versie 1.1 <?= $version; ?>
+      	</span>
+
+      	<span class="right">
+      		
+      		<a href="https://github.com/sgezel/pronostiek"><img class=footer-img" src="img/GitHub-Mark-Light-32px.png" /> </a>
+      	</span>  
+      </div>
+    </footer>
+
  <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
