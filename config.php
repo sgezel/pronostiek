f538b45c6e485a202fe048d8ef021c7b1245f66c
Sandor <sandor@codepage.be>
2018-04-11 14:54:40 +0200

    favicons added settings file added

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$config = [];
+
+$config["version"] = "1.2";
+
+$config["enable_registrations"] = true;
+$config["enable_login"] = true;
+
+
+$config["bonuspoints"] = 2;
+
+$config["question_close_time"] = '2018-04-01 17:00:00';
+$config["close_answer_before_match"] = 1;
+?>
